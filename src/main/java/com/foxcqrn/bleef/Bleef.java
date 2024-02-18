9e244f467e78858e9ed0b0d992a54fe4e38e6dfe
ashduino101 <ashduino101@gmail.com>
2024-02-17 17:41:56 -0800

    fix

--- src/main/java/com/foxcqrn/bleef/Bleef.java
+++ src/main/java/com/foxcqrn/bleef/Bleef.java
@@ -1,6 +1,7 @@
 package com.foxcqrn.bleef;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.Favicon;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -9,6 +10,11 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 import java.util.logging.Level;
 
@@ -23,6 +29,8 @@ public final class Bleef extends Plugin implements Listener {
 
     @Override
     public void onEnable() {
+        getProxy().getPluginManager().registerListener(this, this);
+
         plugin.getLogger().log(Level.INFO, "boofed up");
     }
 
@@ -31,19 +39,21 @@ public final class Bleef extends Plugin implements Listener {
         plugin.getLogger().log(Level.INFO, "boofed down");
     }
 
-    @EventHandler
-    public void onServerPing(ProxyPingEvent event) {
+    @EventHandler(priority = Byte.MAX_VALUE - 1)
+    public void onProxyPing(ProxyPingEvent event) throws IOException {
         Random r = new Random();
         int randomNumber = r.nextInt(PluginUtil.MOTDArray.length);
         ServerPing resp = new ServerPing();
-        BaseComponent component = new ComponentBuilder()
-                .color(ChatColor.LIGHT_PURPLE)
-                .append(PluginUtil.MOTDArray[randomNumber])
-                .append("\n")
-                .color(ChatColor.AQUA)
-                .append(PluginUtil.DiscordInvite)
-                .getComponent(0);
+        BaseComponent component = new ComponentBuilder().append(
+                ChatColor.LIGHT_PURPLE +
+                PluginUtil.MOTDArray[randomNumber] + "\n" +
+                ChatColor.AQUA +
+                PluginUtil.DiscordInvite
+        ).getComponent(0);
         resp.setDescriptionComponent(component);
+        resp.setVersion(event.getResponse().getVersion());
+        InputStream stream = this.getResourceAsStream("favicon.png");
+        resp.setFavicon(Favicon.create(ImageIO.read(stream)));
         event.setResponse(resp);
     }
 }
